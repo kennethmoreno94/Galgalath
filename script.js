9ef1328389533e64778d20cd4de854b546d47045
kennethmoreno94 <kivanmoreno2025@gmail.com>
2026-01-28 14:59:14 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Web de artesanías lista");
+document.addEventListener("scroll", () => {
+  document.querySelectorAll(".section").forEach(section => {
+    const rect = section.getBoundingClientRect();
+    if (rect.top < window.innerHeight * 0.85) {
+      section.style.opacity = 1;
+      section.style.transform = "translateY(0)";
+    }
+  });
+});
