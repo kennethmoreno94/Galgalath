e0bb7c467014030e0ceeb31493462a4c5275918e
kennethmoreno94 <kivanmoreno2025@gmail.com>
2026-01-28 21:40:05 -0600

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,18 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+// Lightbox para imágenes
+const images = document.querySelectorAll(".card img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+
+images.forEach(img => {
+  img.addEventListener("click", () => {
+    lightboxImg.src = img.src;
+    lightbox.style.display = "flex";
+  });
+});
+
+lightbox.addEventListener("click", () => {
+  lightbox.style.display = "none";
+});
